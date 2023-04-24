fd3d25d557646cff0a7bc7539207056538fa5a9e
MikeLavin <mlavin@villanova.edu>
2023-04-24 16:26:53 -0400

    Created search and conquer

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieList = (props) =>{
     return(
         <>
-        {props.movies.map((movie,index) =>
+        {props.movies.map((movie, index) =>
          <div className = 'd-flex justify-content-start m-3'>
             <img src = {movie.Poster} alt= 'movie'></img>
         </div>)}
