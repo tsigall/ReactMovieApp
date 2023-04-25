08d86067bd890a64663b8ffa95292556e1b4e4ae
amithasound <70346133+amithasound@users.noreply.github.com>
2023-04-25 10:58:11 -0400

    added saved to favorite function

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,68 +1,43 @@
 import React, {useEffect, useState} from 'react';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 const App= ()=> {
-  const [movies, setMovies] = useState([
-    {
-    Title: 'Star Wars: Episode IV - A New Hope',
-    Year: '1977',
-    imdbID: 'tt0076759',
-    Type: 'movie',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg'
-},
-{
-    Title: 'Star Wars: Episode V - The Empire Strikes Back',
-    Year: '1980',
-    imdbID: 'tt0080684',
-    Type: 'movie',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'
-},
-{
-  Title: 'Rogue One: A Star Wars Story',
-  Year: '2016',
-  imdbID: 'tt3748528',
-  Type: 'movie',
-  Poster: 'https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg'
-},
-{
-  Title: 'Star Wars: Episode VIII - The Last Jedi',
-  Year: '2017',
-  imdbID: 'tt2527336',
-  Type: 'movie',
-  Poster: 'https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg'
-},
-{
-  Title: 'Star Wars: Episode IX - The Rise of Skywalker',
-  Year: '2019',
-  imdbID: 'tt2527338',
-  Type: 'movie',
-  Poster: 'https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg'
-}
-]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-const getMovieRequest = async() =>{
-  const url = "http://www.omdbapi.com/?s=star wars&apikey=29fda01a"
-
+const getMovieRequest = async(searchValue) =>{
+  const url =  `http://www.omdbapi.com/?s=${searchValue}&apikey=29fda01a`;
+  
   const response = await fetch(url);
   const responseJson = await response.json();
-  console.log(responseJson);
+
+  if(responseJson.Search){
+    setMovies(responseJson.Search);
+  }
+
+
+
 };
+
 useEffect(()=>{
-  getMovieRequest();
-}, [])
-
-
-
-
+  getMovieRequest(searchValue);
+}, [searchValue])
 
 
 
   return(
     <div className='container-fluid movie-app'> 
-      <div className='row'>
-        <MovieList movies = {movies}/>
+      <div className = 'row d-flex align-items-center mt-10 mb-10'>
+        <MovieListHeading heading = 'Movies' />
+        <SearchBox searchValue = {searchValue} setSearchValue = {setSearchValue} />
+      </div>
+      <div className='d-flex justify-content-start m-10'>
+          <MovieList movies = {movies} favoriteComponent = {AddFavorites}/>
       </div>
     </div>
   ); 
